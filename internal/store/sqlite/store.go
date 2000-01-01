7b2dbb234e4ec274ff94f9f71531fceab65a5a6e
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for bd7fd281-74ab-4bc8-a153-3249cea7c433-bug-4

--- internal/store/sqlite/store.go
+++ internal/store/sqlite/store.go
@@ -13,11 +13,14 @@ import (
 type Store struct {
 	db           *sql.DB
 	sharedMemory bool
+	closeMu      sync.Mutex
+	closed       bool
 }
 
 var memoryStorePool struct {
 	sync.Mutex
-	current *Store
+	db       *sql.DB
+	refCount int
 }
 
 func Open(path string) (*Store, error) {
@@ -25,24 +28,16 @@ func Open(path string) (*Store, error) {
 		path = ":memory:"
 	}
 	if path == ":memory:" {
-		memoryStorePool.Lock()
-		defer memoryStorePool.Unlock()
-		if memoryStorePool.current != nil {
-			return memoryStorePool.current, nil
-		}
+		return openSharedMemoryStore()
 	}
-	dsn := path
-	if path == ":memory:" {
-		dsn = "file:bioacoustic?mode=memory&cache=shared"
-	}
-	db, err := sql.Open("sqlite", dsn)
+	db, err := sql.Open("sqlite", path)
 	if err != nil {
 		return nil, fmt.Errorf("打开 SQLite: %w", err)
 	}
 	db.SetMaxOpenConns(8)
 	db.SetMaxIdleConns(4)
 	db.SetConnMaxLifetime(30 * time.Minute)
-	s := &Store{db: db, sharedMemory: path == ":memory:"}
+	s := &Store{db: db, sharedMemory: false}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := s.configure(ctx); err != nil {
@@ -57,9 +52,45 @@ func Open(path string) (*Store, error) {
 		db.Close()
 		return nil, err
 	}
-	if s.sharedMemory {
-		memoryStorePool.current = s
+	return s, nil
+}
+
+// openSharedMemoryStore returns a Store backed by a single in-memory SQLite
+// database shared across callers within the same process. The underlying
+// *sql.DB is reference counted, so closing one Store does not affect other
+// Stores still holding a reference; the database is only closed once the last
+// reference is released.
+func openSharedMemoryStore() (*Store, error) {
+	memoryStorePool.Lock()
+	defer memoryStorePool.Unlock()
+	if memoryStorePool.db != nil {
+		memoryStorePool.refCount++
+		return &Store{db: memoryStorePool.db, sharedMemory: true}, nil
 	}
+	db, err := sql.Open("sqlite", "file:bioacoustic?mode=memory&cache=shared")
+	if err != nil {
+		return nil, fmt.Errorf("打开 SQLite: %w", err)
+	}
+	db.SetMaxOpenConns(8)
+	db.SetMaxIdleConns(4)
+	db.SetConnMaxLifetime(30 * time.Minute)
+	s := &Store{db: db, sharedMemory: true}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := s.configure(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+	if err := s.migrate(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+	if err := s.VerifyIntegrity(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+	memoryStorePool.db = db
+	memoryStorePool.refCount = 1
 	return s, nil
 }
 
@@ -74,12 +105,28 @@ func (s *Store) configure(ctx context.Context) error {
 }
 
 func (s *Store) Close() error {
+	s.closeMu.Lock()
+	defer s.closeMu.Unlock()
+	if s.closed {
+		return nil
+	}
+	s.closed = true
 	if s.sharedMemory {
 		memoryStorePool.Lock()
-		if memoryStorePool.current == s {
-			memoryStorePool.current = nil
+		db := memoryStorePool.db
+		if memoryStorePool.refCount > 0 {
+			memoryStorePool.refCount--
+		}
+		if memoryStorePool.refCount == 0 && db != nil {
+			memoryStorePool.db = nil
+		} else {
+			db = nil
 		}
 		memoryStorePool.Unlock()
+		if db != nil {
+			return db.Close()
+		}
+		return nil
 	}
 	return s.db.Close()
 }
